1cedb77103d1d352641abf612fa87edb2f74ec0b
Onee12 <arielmmartz@gmail.com>
2022-11-09 13:58:49 -0300

    add navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+    let logo = "https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"
+    return <nav>
+    <img src={logo} alt="Logo-poke-api"/>
+    
+    </nav>
+}
+
+export default Navbar;
