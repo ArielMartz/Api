451411b1e7e6e5553c63ec3c2ec6bbc3e85cac5d
Onee12 <arielmmartz@gmail.com>
2022-11-09 23:46:38 -0300

    add useState

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 const Searchbar = () => {
+    const [search, setSearch] = useState('');
+
+    const onChange = (e) => {
+        setSearch(e.target.value);
+
+    }
+
     return(
-        <div>Searchbar</div>
+        <div>
+            <div>
+                <input type="text" placeholder="Buscar pokemon" onChange={onChange}/>
+            </div>
+            <div>{search}</div>
+        </div>
     )
 }
 
