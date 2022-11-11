3fb257a41fb0cfded40514d1fb78640289cd7c04
Onee12 <arielmmartz@gmail.com>
2022-11-10 21:52:54 -0300

    add api

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
-
+import { searchPokemon } from "../../api";
 
 const Searchbar = () => {
     const [search, setSearch] = useState('');
 
     const onChange = (e) => {
+        console.log(e.target.value);
         setSearch(e.target.value);
 
+    }
+
+    const onClick = async (e) => {
+        const data = await searchPokemon(search);
+        console.log(data)
     }
 
     return(
@@ -14,7 +20,7 @@ const Searchbar = () => {
             <div>
                 <input type="text" placeholder="Buscar pokemon" onChange={onChange}/>
             </div>
-            <div>{search}</div>
+            <button onClick={onClick}>Buscar</button>
         </div>
     )
 }
